c66964f85cad814b0ac08b5a384e9a67ae0a927e
Stimim Chen <106878+Stimim@users.noreply.github.com>
2020-10-28 00:32:38 +0800

    define type "AttackModifier"

--- src/app/data-type/attack.ts
+++ src/app/data-type/attack.ts
@@ -1,15 +1,25 @@
 export interface Attack {
-  name: string;
+  name?: string;
   // attack bonus.
   // Multiple hit can be defined by (e.g.) 17/17/12. Then there will be three
   // attack rolls, with attack bonus 17, 17, 12 respectively.
-  attack: string;
+  attack?: string;
   // damage roll.
   // Example: 1d8 + 1 + 2 + 3
-  damage: string;
+  damage?: string;
   // It will be a critical hit if `d20 >= critical_threshold`.
-  crit_threshold: number;
+  crit_threshold?: number;
   // Multiplier to be applied on critical hit.
   // crit_damage = roll(damage) + roll(damage) + ...
-  crit_multiplier: number;
+  crit_multiplier?: number;
+}
+
+export interface AttackModifier {
+  name?: string;
+  attack_bonus?: string;
+  damage_bonus?: string;
+  // This "overrides" the damage bonus on critical hit ("not" adding to it).
+  // If this is empty, the default value is:
+  //    dice_of(damage_bonus) + crit_multiplier * const_of(damage_bonus)
+  damage_bonus_on_crit?: string;
 }
