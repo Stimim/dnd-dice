ec213e0a4937b0bd27a3adce5ca863f27cfeb8f7
Stimim Chen <106878+Stimim@users.noreply.github.com>
2020-11-28 21:21:59 +0800

    0.0.2

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,7 +1,7 @@
 // THIS FILE IS AUTO GENERATED, DO NOT CHANGE IT
 // USE "npm version <major | minor | patch>" INSTEAD
 export const VERSION = {
-  version: "0.0.1",
-  commit: "659657a15c89c3e778c2fa59739ba55235f0e89d",
-  timestamp: 1606408095014,
+  version: "0.0.2",
+  commit: "8c21a565f4f549cc3be11e692a40b796a8a51f6c",
+  timestamp: 1606569719779,
 };
